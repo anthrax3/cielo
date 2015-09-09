ff48e731b234193a546d0c1a91654860f1e29b22
Ramon Barros <contato@ramon-barros.com>
2015-09-09 10:18:02 -0300

    Fix bug session.

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -7,14 +7,16 @@
  * @author   Ramon Barros <contato@ramon-barros.com>
  */
 
-ini_set('display_errors',1);
-error_reporting(E_ALL | E_STRICT );
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
 date_default_timezone_set('America/Sao_Paulo');
 
 define('DS', DIRECTORY_SEPARATOR);
 define('APP_ROOT', realpath(__DIR__.DS.'..'));
+define('EXT', '.php');
 $time = time();
 header('Last-Modified: '.gmdate('D, d M Y H:i:s', $time).'GMT');
+session_start();
 /*
 | ------------------------------------------------- -------------------------
 | Check Extensions
@@ -25,10 +27,14 @@ header('Last-Modified: '.gmdate('D, d M Y H:i:s', $time).'GMT');
 |
 */
 
-if ( ! extension_loaded('curl'))
-{
+if (! extension_loaded('curl')) {
     die('cielo requires the cUrl PHP extension.'.PHP_EOL);
     exit(1);
+}
+
+if (! extension_loaded('ionCube Loader')) {
+    //die('cielo requires the ionCube PHP Loader extension.'.PHP_EOL);
+    //exit(1);
 }
 
 /*
@@ -36,14 +42,14 @@ if ( ! extension_loaded('curl'))
 | Register The Auto Loader
 |--------------------------------------------------------------------------
 |
-| This application is installed by the Composer, 
+| This application is installed by the Composer,
 | that provides a class loader automatically.
 | Use it to seamlessly and feel free to relax.
 |
 */
 
-$composer_autoload = APP_ROOT.DS.'vendor'.DS.'autoload.php';
-if(!file_exists($composer_autoload)){
+$composer_autoload = APP_ROOT . DS . 'vendor' . DS . 'autoload.php';
+if (!file_exists($composer_autoload)) {
     die('Please use the composer to install http://getcomposer.org');
 }
 require $composer_autoload;
--- public/index.php
+++ public/index.php
@@ -25,7 +25,7 @@ use Dso\Cielo\CieloMode;
 
 $mode = Dso\Cielo\CieloMode::DEPLOYMENT;
 
-$cielo = new Dso\Cielo\Cielo( $mode );
+$cielo = new Dso\Cielo\Cielo($mode);
 
 /**
  * Define o código de afiliação.
@@ -34,7 +34,7 @@ $cielo = new Dso\Cielo\Cielo( $mode );
  * Test BuyCielo = 1001734898
  * Test BuyLoja = 1006993069
  */
-$cielo->setAffiliationCode( '1006993069' );
+$cielo->setAffiliationCode('1006993069');
 
 /**
  * Define a chave de afiliação.
@@ -43,19 +43,19 @@ $cielo->setAffiliationCode( '1006993069' );
  * Test BuyCielo = e84827130b9837473681c2787007da5914d6359947015a5cdb2b8843db0fa832
  * Test BuyLoja = 25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3
  */
-$cielo->setAffiliationKey( '25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3' );
+$cielo->setAffiliationKey('25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3');
 
 /**
  * Define a url de retorno
  * O código abaixo é usado para retornar as informações da transação
  */
-$cielo->setReturnURL( 'http://localhost/cielo/public/?mode=retorno' );
+$cielo->setReturnURL('http://localhost/cielo/public/?mode=retorno');
 
 /**
  * Número do pedido
  * @var string
  */
-$orderNumber = substr( str_shuffle( '0123456789' ), 0, 8 );
+$orderNumber = substr(str_shuffle('0123456789'), 0, 8);
 
 /**
  * Valor do pedido, esse valor deve ser um inteiro onde os últimos
@@ -90,7 +90,7 @@ $parcelas = 1;
 if ($paymentProduct == 1 || $paymentProduct == 'A') {
     $parcels = 1;
 } else {
-    $parcels = (int)$parcelas;
+    $parcels = (int) $parcelas;
 }
 
 /**
@@ -100,11 +100,11 @@ if ($paymentProduct == 1 || $paymentProduct == 'A') {
  */
 if (empty($_REQUEST['mode'])) {
     define('BUYMODE', 'BuyLoja');
-} else if ($_REQUEST['mode'] == 'retorno') {
+} elseif ($_REQUEST['mode'] == 'retorno') {
     define('BUYMODE', 'retorno');
-} else if ($_REQUEST['mode'] == 'consulta') {
+} elseif ($_REQUEST['mode'] == 'consulta') {
     define('BUYMODE', 'consulta');
-} else if ($_REQUEST['mode'] == 'cielo') {
+} elseif ($_REQUEST['mode'] == 'cielo') {
     define('BUYMODE', 'BuyCielo');
 }
 
@@ -139,9 +139,9 @@ $cardExpiration = '201806'; //preg_replace('/\D/', '', $_POST['cardExpiration'])
 
 $_POST['securityCode'] = 123;
 
-if(isset($_POST['securityCode'])){
+if (isset($_POST['securityCode'])) {
     $securityCode = preg_replace('/\D/', '', $_POST['securityCode']);
-}else{
+} else {
     $securityCode = null;
 }
 
@@ -150,16 +150,11 @@ if(isset($_POST['securityCode'])){
  * Indicador do código de segurança
  * @var integer
  */
-if ($securityCode == null || $securityCode == "")
-{
+if ($securityCode == null || $securityCode == "") {
     $indicator = 0;
-}
-else if ($cartao == Dso\Cielo\CreditCard::MASTER_CARD)
-{
+} elseif ($cartao == Dso\Cielo\CreditCard::MASTER_CARD) {
     $indicator = 1;
-}
-else
-{
+} else {
     $indicator = 1;
 }
 
@@ -167,7 +162,7 @@ switch (BUYMODE) {
     case 'BuyCielo':
         var_dump('BuyCielo');
         $transaction = $cielo->automaticCapture()
-                             ->buildTransactionRequest( $cartao , $orderNumber , $orderValue , $paymentProduct , $parcels )
+                             ->buildTransactionRequest($cartao, $orderNumber, $orderValue, $paymentProduct, $parcels)
                              ->call();
         /**
          * Dados da autorização
@@ -177,10 +172,10 @@ switch (BUYMODE) {
 
         echo "<pre>";
 
-        var_dump( $transaction->getPan() );
-        var_dump( $transaction->getStatus() );
-        var_dump( $transaction->getTID() );
-        var_dump( $cielo->__getLastResponse() ); //Retorna o XML de resposta
+        var_dump($transaction->getPan());
+        var_dump($transaction->getStatus());
+        var_dump($transaction->getTID());
+        var_dump($cielo->__getLastResponse()); //Retorna o XML de resposta
 
         echo "</pre>";
 
@@ -188,7 +183,7 @@ switch (BUYMODE) {
     case 'consulta':
         var_dump('consulta');
         $tid = empty($_SESSION['tid']) ? $_REQUEST['tid'] : $_SESSION['tid'];
-        $transaction = $cielo->buildQueryTransaction( $tid )->call();
+        $transaction = $cielo->buildQueryTransaction($tid)->call();
 
         echo "<pre>";
             var_dump($cielo->__getLastRequest()); //Recupera o XML de requisição
@@ -200,11 +195,10 @@ switch (BUYMODE) {
         echo "<pre>";
         try {
             $tid = empty($_SESSION['tid']) ? $_REQUEST['tid'] : $_SESSION['tid'];
-            $transaction = $cielo->buildQueryTransaction( $tid )->call();
+            $transaction = $cielo->buildQueryTransaction($tid)->call();
             var_dump($transaction->getStatus());
             if ($transaction->getStatus() == 3 || $transaction->getStatus() == 4) {
-
-                $transaction = $cielo->buildAuthorizationTIDRequest( $transaction->getTID() )->call();
+                $transaction = $cielo->buildAuthorizationTIDRequest($transaction->getTID())->call();
 
                 if ($transaction->getStatus() == 6) {
                     var_dump('Trasação Capturada');
@@ -231,7 +225,7 @@ switch (BUYMODE) {
          * mais de uma vez, caso um timeout de conexão ou algum problema de rede ocorra
          * @var string
          */
-        $tid = $cielo->buildTIDRequest( $cartao , $paymentProduct )->call()->getTID();
+        $tid = $cielo->buildTIDRequest($cartao, $paymentProduct)->call()->getTID();
 
         /**
          * Define se será feita a autorização automática, seu valor pode ser um dos seguinte:
@@ -240,7 +234,7 @@ switch (BUYMODE) {
          * 2 – Autorizar autenticada e não autenticada. O cliente é redirecionado para a operadora do cartão
          * 3 – Autorizar sem passar por autenticação (somente para crédito). O cliente não é redirecionado para a operadora do cartão
          */
-        $cielo->setAuthorization( 2 );
+        $cielo->setAuthorization(2);
 
         /**
          * Cria a transação com autenticaçao dentro da loja, fazendo captura automática
@@ -248,7 +242,7 @@ switch (BUYMODE) {
          */
         $transaction = $cielo
                 ->automaticCapture()
-                ->buildAuthenticationRequest( $cartao , $cardNumber , $cardExpiration , $indicator , $securityCode , $holderName, $orderNumber , $orderValue , $paymentProduct , $parcels , null )
+                ->buildAuthenticationRequest($cartao, $cardNumber, $cardExpiration, $indicator, $securityCode, $holderName, $orderNumber, $orderValue, $paymentProduct, $parcels, null)
                 ->call();
 
         /**
@@ -267,7 +261,6 @@ switch (BUYMODE) {
         var_dump( $authentication->getValue() );
         */
 
-
         /**
          * Cria a transação com autorização dentro da loja, fazendo captura automática
          * @var Transaction
@@ -284,7 +277,6 @@ switch (BUYMODE) {
          * @var AuthorizationNode
          */
         //$authorization = $transaction->getAuthorization();
-
 
         //var_dump($authorization);
 
